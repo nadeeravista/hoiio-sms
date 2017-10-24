c7994eacadb01fce5290147c20369519489df60b
nadeeraktp <nadeera@smartcitysolutions.com.sg>
2017-10-24 16:38:27 +0800

    Change folder structure

---
+++ src/IVRCallState.php
@@ -0,0 +1,8 @@
+<?php
+namespace Kalahe\HoiioSms;
+
+class IVRCallState {
+    const RINGING = "ringing";
+    const ONGOING = "ongoing";
+    const ENDED = "ended";
+}
---
+++ src/SMSStatus.php
@@ -0,0 +1,13 @@
+<?php
+namespace Kalahe\HoiioSms;
+
+/**
+ * Possible delivery status of SMS sent previously
+ */
+class SMSStatus {
+    const QUEUED = "queued";
+    const DELIVERED = "delivered";
+    const FAILED = "failed";
+    const ERROR = "error";
+}
+
