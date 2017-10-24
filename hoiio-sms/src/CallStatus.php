560e258e8ad13c52fd85fc37e4a9b7abc0d0cceb
nadeeraktp <nadeera@smartcitysolutions.com.sg>
2017-10-24 15:29:21 +0800

    Innitial commit by Kalahe

---
+++ hoiio-sms/src/CallStatus.php
@@ -0,0 +1,15 @@
+<?php
+namespace Kalahe\HoiioSms;
+
+/**
+ * Possible dial status of a number in a call.
+ */
+class CallStatus {
+    const ANSWERED = 'answered';
+    const UNANSWERED = 'unanswered';
+    const FAILED = 'failed';
+    const BUSY = 'busy';
+    const ONGOING = 'ongoing';
+}
+
+
---
+++ hoiio-sms/src/routes.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Kalahe\HoiioSms;
+
+/**
+ * Description of routes
+ *
+ * @author nadeerathilak
+ */
+class routes {
+    //put your code here
+}
